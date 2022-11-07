d716162f9c1de8c12a080d58bbbe0217c43905de
Mauro Evangelista <mauroevangelista08@gmail.com>
2022-11-06 21:12:17 -0300

    Fin de la Seccion 6 del curso. App completa.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-); 
-test1
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+class App extends React.Component{
+
+    // Método para iniciar variables, estados, etc
+    // NO utilizar este método para cargar datos, llamadas a API's, etc.
+   /* constructor(props){
+        super(props);
+        
+        // THIS IS THE ONLY TIME we do direct assignment 
+        // to this.state
+        this.state = {lat: null, errorMessage: ''};
+
+        // No usar el método constructor() para setear 'state'
+    }*/
+
+    state = {lat: null, errorMessage: ''};
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(            
+            // llamo a setState para actualizar el estado de la clase App para que se auto renderice nuevamente
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState( { errorMessage: err.message } )           
+        );
+    }
+/*    componentDidUpdate(){
+        console.log("My component was just updated - it rerendered!");
+    }*/
+
+    renderContent(){
+             /* return (
+            <div>
+                Latitude: {this.state.lat}
+                <br/>
+                Error: {this.state.errorMessage}
+            </div>
+        );*/
+
+        if (this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+        if (!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay  lat={this.state.lat} />;
+        }
+
+        return <Spinner message="Please, accept location request" />;
+
+    }
+
+    // React says we have to define render!!
+    render() {
+        
+      return <div className="border red">{this.renderContent()}</div>;
+         
+    }
+
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
